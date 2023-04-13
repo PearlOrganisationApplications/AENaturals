22528160359b4b9fdfd47236c99fc8c58c578efd
Pearl <sudhanshu@pearlorganisation.com>
2023-04-13 18:43:11 +0530

    Salesman dashboard

--- app/src/main/java/com/aenatural/aenaturals/common/Splash.kt
+++ app/src/main/java/com/aenatural/aenaturals/common/Splash.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
 import androidx.core.content.ContextCompat
 import com.pearl.aenaturals.R
 
@@ -12,6 +13,7 @@ class Splash : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         window.statusBarColor = ContextCompat.getColor(this, R.color.lightgreen)
+        window.decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         Handler().postDelayed({
             val mIntent = Intent(this@Splash, WelcomeScreen::class.java)
             startActivity(mIntent)
--- app/src/main/java/com/aenatural/aenaturals/salesmans/SalesmanDashboard.kt
+++ app/src/main/java/com/aenatural/aenaturals/salesmans/SalesmanDashboard.kt
@@ -2,11 +2,17 @@ package com.aenatural.aenaturals.salesmans
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.content.ContextCompat
 import com.pearl.aenaturals.R
 
 class SalesmanDashboard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.statusBarColor = ContextCompat.getColor(this, R.color.lightgreen)
+        window.decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         setContentView(R.layout.activity_salesman_dashboard)
+
+
     }
 }
