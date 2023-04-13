16fccdfe2e1f2fd791fec097277e8aad3754f50c
Pearl <sudhanshu@pearlorganisation.com>
2023-04-13 16:09:17 +0530

    Login screen design

---
+++ app/src/main/java/com/pearl/aenaturals/UI/Activity/Splash.kt
@@ -0,0 +1,21 @@
+package com.pearl.aenaturals.UI.Activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import androidx.core.content.ContextCompat
+import com.pearl.aenaturals.R
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        window.statusBarColor = ContextCompat.getColor(this, R.color.lightgreen)
+        Handler().postDelayed({
+            val mIntent = Intent(this@Splash, Login::class.java)
+            startActivity(mIntent)
+            finish()
+        }, 2000)
+    }
+}
