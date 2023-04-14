ce7f0b6c4da8c5f66aa038356a021d0c22054dbe
Pearl Organisation <rajat@pearlorganisation.com>
2023-04-14 11:32:39 +0530

    afea

--- app/src/main/java/com/aenatural/aenaturals/common/Login.kt
+++ app/src/main/java/com/aenatural/aenaturals/common/Login.kt
@@ -3,6 +3,7 @@ package com.aenatural.aenaturals.common
 import android.content.Intent
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
@@ -20,6 +21,7 @@ class Login : AppCompatActivity() {
     lateinit var salesmanButton: CardView
     lateinit var retailerButton: CardView
     lateinit var distributorButton: CardView
+    lateinit var tv_login: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,10 +39,30 @@ class Login : AppCompatActivity() {
         salesmanButton = findViewById(R.id.salesmanButton)
         retailerButton = findViewById(R.id.retailerButton)
         distributorButton = findViewById(R.id.distributorButton)
+        tv_login = findViewById(R.id.tv_login)
     }
 
 
     private fun clickListeners() {
+
+        tv_login.setOnClickListener {
+
+
+            if(emailEditText.text.toString().equals("salesman") && passwordEditText.text.toString().equals("123")){
+                startActivity(Intent(this, SalesmanDashboard::class.java))
+            }else if(){
+
+            }else if(){
+
+            }else{
+
+            }
+
+
+        }
+
+
+
         salesmanButton.setOnClickListener {
             startActivity(Intent(this, SalesmanDashboard::class.java))
         }
