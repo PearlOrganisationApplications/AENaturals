de430e9106de313d8e6b05e470f46d4198a92d54
Pearl <sudhanshu@pearlorganisation.com>
2023-04-19 14:34:05 +0530

    Responsive

--- app/src/main/java/com/aenatural/aenaturals/salesmans/SalesmanProfileActivity.kt
+++ app/src/main/java/com/aenatural/aenaturals/salesmans/SalesmanProfileActivity.kt
@@ -1,16 +1,28 @@
 package com.aenatural.aenaturals.salesmans
 
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
+import android.widget.TextView
+import android.widget.Toast
 import com.aenatural.aenaturals.baseframework.BaseClass
 import com.pearl.aenaturals.R
 
+
 class SalesmanProfileActivity : BaseClass() {
+
+    lateinit var backTV:TextView
+    lateinit var salesmanLogout:TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setLayoutXml()
-
+        initializeViews()
+        initializeClickListners()
     }
 
     override fun setLayoutXml() {
@@ -21,14 +33,41 @@ class SalesmanProfileActivity : BaseClass() {
     }
 
     override fun initializeViews() {
+        backTV = findViewById(R.id.backTV)
+        salesmanLogout = findViewById(R.id.salesmanLogout)
     }
 
     override fun initializeClickListners() {
-    }
+        backTV.setOnClickListener {
+            onBackPressed()
+        }
+
+        salesmanLogout.setOnClickListener {
+            var alertDialog = AlertDialog.Builder(this)
+            alertDialog.setTitle("Alert")
+            alertDialog.setMessage("Do you want to Logout?")
+            alertDialog.setPositiveButton("Yes"){dialogInterface,_ ->
+                run{
+                    startActivity(Intent(this, SalesmanDashboard::class.java))
+                    dialogInterface.dismiss()
+                }
+            }
+            alertDialog.setNegativeButton("No"){dialogInterface,_ ->
+                run{
+                    //startActivity(Intent(this, SalesmanDashboard::class.java))
+                    dialogInterface.dismiss()
+                }
+            }
+            alertDialog.show()
+    }}
 
     override fun initializeInputs() {
+
     }
 
     override fun initializeLabels() {
+
     }
+
+
 }
