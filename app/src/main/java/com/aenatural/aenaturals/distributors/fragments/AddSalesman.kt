819927efad5cd2b310f3a42bfbd317a78295d3fd
Simranjeet Singh <erajat.rs.rs@gmail.com>
2023-07-12 13:03:13 +0530

    gnfg

--- app/src/main/java/com/aenatural/aenaturals/distributors/fragments/AddSalesman.kt
+++ app/src/main/java/com/aenatural/aenaturals/distributors/fragments/AddSalesman.kt
@@ -1,23 +1,35 @@
 package com.aenatural.aenaturals.distributors.fragments
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
+import androidx.annotation.RequiresApi
 import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import com.aenatural.aenaturals.R
+import com.aenatural.aenaturals.baseframework.BaseFragment
 import com.aenatural.aenaturals.distributors.DistributorDashboard
 import com.aenatural.aenaturals.salesmans.fragments.ProductsFragment
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.yalantis.ucrop.UCrop
 
-class AddSalesman : Fragment() {
+class AddSalesman : BaseFragment() {
 
     private lateinit var aadhaarfront: LinearLayout
     private lateinit var aadhaarback: LinearLayout
+    private lateinit var aadhaarbackPic: ImageView
+    private lateinit var aadhaarfrontPic: ImageView
     private lateinit var distributorFormSubmit: CardView
     companion object {
 
@@ -36,6 +48,7 @@ class AddSalesman : Fragment() {
         return inflater.inflate(R.layout.fragment_add_salesman, container, false)
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         backPress()
@@ -66,10 +79,18 @@ class AddSalesman : Fragment() {
     }
 
 
+    @RequiresApi(Build.VERSION_CODES.M)
     private fun initClickListener(view: View) {
-        aadhaarfront = view.findViewById(R.id.dist_adharFrontLL)
-        aadhaarback = view.findViewById(R.id.dist_adharRearLL)
-        aadhaarfront.setOnClickListener {  }
+
+
+        aadhaarfrontPic.setOnClickListener {
+        imageType=1
+            requestCameraPermission()
+        }
+        aadhaarback.setOnClickListener{
+            imageType=2
+            requestCameraPermission()
+        }
        /* var m = 0
         var f = 0
         distributor_profile_pic.setOnClickListener {
@@ -97,11 +118,119 @@ class AddSalesman : Fragment() {
 
     }
 
+    /*override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                REQUEST_IMAGE_CAPTURE -> {
+                    val imageBitmap = data?.extras?.get("data") as Bitmap
+                    val imageUri = saveImageToFile(imageBitmap)
+                    if (imageUri != null) {
+                        cropImage(imageUri)
+                    }
+                }
+                REQUEST_IMAGE_GALLERY -> {
+                    val imageUri = data?.data
+                    if (imageUri != null) {
+                        cropImage(imageUri)
+                    }
+                }
+                UCrop.REQUEST_CROP -> {
+                    val resultUri = UCrop.getOutput(data!!)
+                    if (resultUri != null) {
+                        when (imageType) {
+                            1  -> {
+                                Glide.with(this)
+                                    .load(resultUri)
+                                    .diskCacheStrategy(DiskCacheStrategy.NONE)
+                                    .skipMemoryCache(true)
+                                    .into(aadhaarfrontPic)
+
+                                Log.d("image111",resultUri.toString())
+
+                            }
+                            2 -> {
+                                Glide.with(this)
+                                    .load(resultUri)
+                                    .diskCacheStrategy(DiskCacheStrategy.NONE)
+                                    .skipMemoryCache(true)
+                                    .into(aadhaarbackPic)
+
+                            }
+                        }
+                    }
+                }
+
+
+            }
+        } else if (resultCode == UCrop.RESULT_ERROR) {
+            val error = UCrop.getError(data!!)
+            // Handle the cropping error
+            Log.e("Error", "Crop error: $error")
+        }
+    }*/
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK) {
+
+            when (requestCode) {
+                REQUEST_IMAGE_CAPTURE -> {
+                    val imageBitmap = data?.extras?.get("data") as Bitmap
+                    val imageUri = saveImageToFile(imageBitmap)
+                    if (imageUri != null) {
+                        cropImage(imageUri)
+                    }
+                }
+                REQUEST_IMAGE_GALLERY -> {
+                    val imageUri = data?.data
+                    if (imageUri != null) {
+                        cropImage(imageUri)
+                    }
+                }
+                UCrop.REQUEST_CROP -> {
+                    val resultUri = UCrop.getOutput(data!!)
+                    if (resultUri != null) {
+                        when (imageType) {
+                            1 -> {
+                                Glide.with(this)
+                                    .load(resultUri)
+                                    .diskCacheStrategy(DiskCacheStrategy.NONE)
+                                    .skipMemoryCache(true)
+                                    .into(aadhaarfrontPic)
+
+                                Glide.with(this)
+                                    .load(resultUri)
+                                    .diskCacheStrategy(DiskCacheStrategy.NONE)
+                                    .skipMemoryCache(true)
+                                    .into(aadhaarfrontPic)
+                            }
+                        }
+                    }
+                }
+            }
+
+        } else if (resultCode == UCrop.RESULT_ERROR) {
+            val error = UCrop.getError(data!!)
+            // Handle the cropping error
+            Log.e("Error", "Crop error: $error")
+        }
+    }
+
     private fun initViews(view: View) {
         /*distributor_profile_pic = view.findViewById(R.id.distributor_profile_pic)
         distributor_parlorpic = view.findViewById(R.id.distributor_parlorpic)*/
         distributorFormSubmit = view.findViewById(R.id.distributorFormSubmit)
+        aadhaarfront = view.findViewById(R.id.dist_adharFrontLL)
+        aadhaarback = view.findViewById(R.id.dist_adharRearLL)
+        aadhaarbackPic = view.findViewById(R.id.dist_adharRearIV)
+        aadhaarfrontPic = view.findViewById(R.id.dist_adharFrontIV)
     }
+
+
 
         override fun onResume() {
         super.onResume()
