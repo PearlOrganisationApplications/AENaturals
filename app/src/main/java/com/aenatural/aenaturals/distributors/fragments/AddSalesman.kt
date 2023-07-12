7f9c4aa267ce9656785a0606c634012687a039cd
Simranjeet Singh <erajat.rs.rs@gmail.com>
2023-07-12 17:33:05 +0530

    gnfgnbv

--- app/src/main/java/com/aenatural/aenaturals/distributors/fragments/AddSalesman.kt
+++ app/src/main/java/com/aenatural/aenaturals/distributors/fragments/AddSalesman.kt
@@ -3,38 +3,33 @@ package com.aenatural.aenaturals.distributors.fragments
 import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.util.Base64
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.activity.OnBackPressedCallback
 import androidx.annotation.RequiresApi
 import androidx.cardview.widget.CardView
-import androidx.fragment.app.Fragment
 import com.aenatural.aenaturals.R
 import com.aenatural.aenaturals.apiservices.AddSalemanApiService
-import com.aenatural.aenaturals.apiservices.CheckoutApiService
 import com.aenatural.aenaturals.apiservices.datamodels.AddSalemanModel
 import com.aenatural.aenaturals.apiservices.datamodels.NormalDataModel
 import com.aenatural.aenaturals.baseframework.BaseFragment
 import com.aenatural.aenaturals.baseframework.Session
 import com.aenatural.aenaturals.common.RetrofitClient
-import com.aenatural.aenaturals.distributors.DistributorDashboard
-import com.aenatural.aenaturals.salesmans.fragments.ProductsFragment
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.yalantis.ucrop.UCrop
-import okhttp3.MediaType.Companion.toMediaType
-import okhttp3.RequestBody.Companion.toRequestBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.io.ByteArrayOutputStream
 
 class AddSalesman : BaseFragment() {
 
@@ -42,9 +37,27 @@ class AddSalesman : BaseFragment() {
     private lateinit var aadhaarback: LinearLayout
     private lateinit var aadhaarbackPic: ImageView
     private lateinit var aadhaarfrontPic: ImageView
+    private lateinit var EmailText: EditText
+    private lateinit var ContactText: EditText
+    private lateinit var AddressText: EditText
+    private lateinit var Pan_noText: EditText
+    private lateinit var male: RadioButton
+    private lateinit var female: RadioButton
+    private lateinit var others: RadioButton
+    private lateinit var email: String
+    private lateinit var contact: String
+    private lateinit var address: String
+    private lateinit var pan_no: String
+    private lateinit var gender: String
+    private lateinit var resultUri: Uri
+    private var image_base64_result: String = RetrofitClient.imageString
+    private lateinit var bitmap: Bitmap
+    private lateinit var aadhar_front: String
+    private lateinit var aadhar_back: String
     private lateinit var Submit: TextView
     lateinit var session: Session
     private lateinit var distributorFormSubmit: CardView
+
     companion object {
 
     }
@@ -67,7 +80,7 @@ class AddSalesman : BaseFragment() {
         super.onViewCreated(view, savedInstanceState)
         backPress()
         session = Session(requireContext())
-        requireActivity().findViewById<LinearLayout>(R.id.headerdistributor).visibility =View.GONE
+        requireActivity().findViewById<LinearLayout>(R.id.headerdistributor).visibility = View.GONE
         initViews(view)
         initClickListener(view)
         initDataModels(view)
@@ -75,15 +88,19 @@ class AddSalesman : BaseFragment() {
     }
 
     private fun backPress() {
-        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
-            override fun handleOnBackPressed() {
-                requireActivity().supportFragmentManager.beginTransaction()
-                    .replace(R.id.DashboardFrameLayout,
-                        DistributorHomeFrag()
-                    ).commit()
-            }
-        })
+        requireActivity().onBackPressedDispatcher.addCallback(
+            viewLifecycleOwner,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    requireActivity().supportFragmentManager.beginTransaction()
+                        .replace(
+                            R.id.DashboardFrameLayout,
+                            DistributorHomeFrag()
+                        ).commit()
+                }
+            })
     }
+
     private fun initRecyclerAdapter(view: View) {
 
     }
@@ -99,63 +116,84 @@ class AddSalesman : BaseFragment() {
 
 
         aadhaarfrontPic.setOnClickListener {
-            imageType=1
+            imageType = 1
             requestCameraPermission()
         }
-        aadhaarback.setOnClickListener{
-            imageType=2
+        aadhaarback.setOnClickListener {
+            imageType = 2
             requestCameraPermission()
-        }
-        Submit.setOnClickListener {
-            callApi()
-        }
-       /* var m = 0
-        var f = 0
-        distributor_profile_pic.setOnClickListener {
-            m = 1
-            if(f==1)
-            {
-                f=0
-                distributor_parlorpic.setBackgroundResource(R.drawable.loginedittextbg)
-            }
-            distributor_profile_pic.setBackgroundResource(R.drawable.tapcurvedbackground)
-        }
-        distributor_parlorpic.setOnClickListener {
-            f=1
-            if(m==1)
-            {
-                m=0
-                distributor_profile_pic.setBackgroundResource(R.drawable.loginedittextbg)
-            }
-            distributor_parlorpic.setBackgroundResource(R.drawable.tapcurvedbackground)
         }
 
-        distributorFormSubmit.setOnClickListener {
-            startActivity(Intent(requireContext(), DistributorDashboard::class.java))
-        }*/
+        Submit.setOnClickListener {
+            email = EmailText.text.toString().trim()
+            contact = ContactText.text.toString().trim()
+            address = AddressText.text.toString().trim()
+            pan_no = Pan_noText.text.toString().trim()
+            if (male.isChecked) {
+                gender = "male"
+            }else if (female.isChecked) {
+                gender = "female"
+            }else{
+                gender = "other"
+            }
+
+            callApi()
+        }
+        /* var m = 0
+         var f = 0
+         distributor_profile_pic.setOnClickListener {
+             m = 1
+             if(f==1)
+             {
+                 f=0
+                 distributor_parlorpic.setBackgroundResource(R.drawable.loginedittextbg)
+             }
+             distributor_profile_pic.setBackgroundResource(R.drawable.tapcurvedbackground)
+         }
+         distributor_parlorpic.setOnClickListener {
+             f=1
+             if(m==1)
+             {
+                 m=0
+                 distributor_profile_pic.setBackgroundResource(R.drawable.loginedittextbg)
+             }
+             distributor_parlorpic.setBackgroundResource(R.drawable.tapcurvedbackground)
+         }
+
+         distributorFormSubmit.setOnClickListener {
+             startActivity(Intent(requireContext(), DistributorDashboard::class.java))
+         }*/
 
     }
 
     private fun callApi() {
+
         val apiService = RetrofitClient.retrofit.create(AddSalemanApiService::class.java)
-        val dataModal: AddSalemanModel = AddSalemanModel(name, job)
-        val call: Call<NormalDataModel> = apiService.addSaleman("Bearer "+session.token.toString(),dataModal)
-
-        call.enqueue(object : Callback<NormalDataModel?> {
-            override fun onResponse(
-                call: Call<NormalDataModel?>,
-                response: Response<NormalDataModel?>
-            ) {
-
-            }
-
-            override fun onFailure(call: Call<NormalDataModel?>, t: Throwable) {
-
-            }
-
-        })
+        val dataModal =
+            AddSalemanModel(
+                email, contact, address, pan_no, gender,
+                       image_base64_result,
+                        image_base64_result)
 
 
+        apiService.addSaleman("Bearer ${session.token}", dataModal)
+            .enqueue(object : Callback<NormalDataModel> {
+                override fun onResponse(
+                    call: Call<NormalDataModel>,
+                    response: Response<NormalDataModel>
+                ) {
+
+                    Log.d("addsaleman", response.body()!!.message.toString())
+                }
+
+                override fun onFailure(call: Call<NormalDataModel>, t: Throwable) {
+                    Toast.makeText(requireContext(), t.stackTraceToString(), Toast.LENGTH_SHORT)
+                        .show()
+                    Log.d("addsaleman1221",t.localizedMessage.toString() )
+                }
+
+
+            })
 
 
     }
@@ -234,7 +272,10 @@ class AddSalesman : BaseFragment() {
                     }
                 }
                 UCrop.REQUEST_CROP -> {
-                    val resultUri = UCrop.getOutput(data!!)
+                    resultUri = UCrop.getOutput(data!!)!!
+                    val imageStream = requireActivity().contentResolver.openInputStream(resultUri);
+                    bitmap = BitmapFactory.decodeStream(imageStream);
+                    image_base64_result = encodeImage(bitmap)!!
                     if (resultUri != null) {
                         when (imageType) {
                             1 -> {
@@ -262,6 +303,13 @@ class AddSalesman : BaseFragment() {
         }
     }
 
+    private fun encodeImage(bm: Bitmap): String? {
+        val baos = ByteArrayOutputStream()
+        bm.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val b: ByteArray = baos.toByteArray()
+        return Base64.encodeToString(b, Base64.DEFAULT)
+    }
+
     private fun initViews(view: View) {
         /*distributor_profile_pic = view.findViewById(R.id.distributor_profile_pic)
         distributor_parlorpic = view.findViewById(R.id.distributor_parlorpic)*/
@@ -272,19 +320,27 @@ class AddSalesman : BaseFragment() {
         aadhaarfrontPic = view.findViewById(R.id.dist_adharFrontIV)
         aadhaarfrontPic = view.findViewById(R.id.dist_adharFrontIV)
         Submit = view.findViewById(R.id.distributor_submitBT)
+        EmailText = view.findViewById(R.id.email)
+        AddressText = view.findViewById(R.id.address)
+        ContactText = view.findViewById(R.id.contact)
+        Pan_noText = view.findViewById(R.id.pan_no)
+        male = view.findViewById(R.id.rbMale)
+        female = view.findViewById(R.id.rbFemale)
+        others = view.findViewById(R.id.rbOthers)
     }
 
 
-
-        override fun onResume() {
+    override fun onResume() {
         super.onResume()
         requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
     }
+
     private val callback = object : OnBackPressedCallback(true) {
         override fun handleOnBackPressed() {
             // Handle the back button press here
 //            val transaction = requireActivity().supportFragmentManager.beginTransaction()
-            requireActivity().supportFragmentManager.beginTransaction().replace(R.id.DashboardFrameLayout,
+            requireActivity().supportFragmentManager.beginTransaction().replace(
+                R.id.DashboardFrameLayout,
                 DistributorHomeFrag()
             ).commit()
             Toast.makeText(requireContext(), "Back button pressed", Toast.LENGTH_LONG).show()
